13e337672a67f2a868f046c5d42170874373ae32
maxkram <max.kramer@live.com>
2017-03-12 17:06:32 +0300

    добавил тесты для GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
+const todos = [{
+  text: "Первый тест задачи"
+}, {
+  text: "Второй тест задачи"
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -23,7 +31,7 @@ describe("POST /todos", () => {
         return done(err);
       }
 
-      Todo.find().then((todos)=> {
+      Todo.find({text}).then((todos)=> {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -40,9 +48,20 @@ describe("POST /todos", () => {
         return done(err);
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
+  });
+});
+describe("GET /todos", () => {
+  it("получит все задачи", (done) => {
+    request(app)
+    .get("/todos")
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   });
 });
