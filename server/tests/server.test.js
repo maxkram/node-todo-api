e4a4886ec04509cc62310069f7c64c8d46ab4316
maxkram <max.kramer@live.com>
2017-03-12 19:38:37 +0300

    добавил тесты для GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require("expect");
 const request = require("supertest");
+const {ObjectID} = require("mongodb");
 
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
 const todos = [{
+  _id: new ObjectID(),
   text: "Первый тест задачи"
 }, {
+  _id: new ObjectID(),
   text: "Второй тест задачи"
 }];
 
@@ -62,6 +65,31 @@ describe("GET /todos", () => {
     .expect((res) => {
       expect(res.body.todos.length).toBe(2);
     })
+    .end(done);
+  });
+});
+describe("GET /todos/:id", ()=>{
+  it("вернет doc задачи", (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+  it("вернет 404, если не найдется задача", (done) => {
+      var hexId = new ObjectID().toHexString();
+
+      request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it("вернет 404 id", (done) => {
+    request(app)
+    .get("/todos/123abc")
+    . expect(404)
     .end(done);
   });
 });
